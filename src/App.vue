29b238a24a57619f03788b7aac0f7f578b506f5d
ilyes hamrouni <ilyes_dev@outlook.com>
2018-12-05 16:39:04 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductList></ProductList>   <!-- this is the productList in use-->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ProductList from './components/Product/ProductList/ProductList.vue' // we should import the Product list so we can use it,
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    ProductList    //after import you define your component here 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Product/ProductList/ProductList.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Hello This is ProductList</h1>
+</template>
+<script>
+export default {
+  name: 'ProductList',  //this is the tag can so you can call it later, example: <ProductList></ProductList> now go see app.vue
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
